f56394a4aa234e2047ef5030b6d4db82c6466349
Keith Crooks <keithcrooks@gmail.com>
2025-11-25 12:42:53 +0000

    Fix incorrect TextTypes

--- src/textnode.py
+++ src/textnode.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 class TextType(Enum):
-    PLAIN = "plain"
+    TEXT = "plain"
     BOLD = "bold"
     ITALIC = "italic"
     CODE = "code"
     LINK = "link"
-    INLINE = "image"
+    IMAGE = "image"
 
 class TextNode():
     def __init__(self, text, text_type, url = None):
