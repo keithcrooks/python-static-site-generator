ff89515a25000fe296eb10a332142514a76f64b2
Keith Crooks <keithcrooks@gmail.com>
2025-11-26 10:15:57 +0000

    Add text_to_textnodes function and tests

---
+++ src/test_text_to_textnodes.py
@@ -0,0 +1,26 @@
+import unittest
+
+from text_to_textnodes import *
+from textnode import *
+
+class TestTextToTextNodes(unittest.TestCase):
+    def test_the_all_the_things(self):
+        text = "This is **text** with an _italic_ word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+
+        result = text_to_textnodes(text)
+
+        self.assertEqual(
+            result,
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("obi wan image", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ]
+        )
---
+++ src/text_to_textnodes.py
@@ -0,0 +1,13 @@
+from split_nodes import *
+from textnode import TextNode, TextType
+
+def text_to_textnodes(text: str):
+    nodes = [TextNode(text, TextType.TEXT)]
+
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    return nodes
