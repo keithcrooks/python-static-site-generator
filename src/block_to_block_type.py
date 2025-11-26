8d9c9cc41c64dd8d5754defca887e496451a6424
Keith Crooks <keithcrooks@gmail.com>
2025-11-26 15:01:19 +0000

    Add block_to_block_type function and tests

---
+++ src/block_to_block_type.py
@@ -0,0 +1,50 @@
+import re
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+HEADING_REGEX = re.compile(r"^#{1,6} [^\n]+$")
+
+def block_to_block_type(block: str) -> BlockType:
+    if is_heading(block):
+        return BlockType.HEADING
+    
+    if is_code(block):
+        return BlockType.CODE
+    
+    if is_quote(block):
+        return BlockType.QUOTE
+    
+    if is_ordered_list(block):
+        return BlockType.ORDERED_LIST
+    
+    if is_unordered_list(block):
+        return BlockType.UNORDERED_LIST
+    
+    return BlockType.PARAGRAPH
+
+def is_code(block: str) -> bool:
+    return block.startswith("```") and block.endswith("```")
+
+def is_heading(block: str) -> bool:
+    return bool(HEADING_REGEX.fullmatch(block))
+
+def is_quote(block :str) -> bool:
+    return all(line.startswith('> ') for line in block.splitlines())
+
+def is_ordered_list(block :str) -> bool:
+    lines = block.splitlines()
+    for i, line in enumerate(lines, start=1):
+        if not line.startswith(f"{i}. "):
+            return False
+        
+    return True
+
+def is_unordered_list(block :str) -> bool:
+    return all(line.startswith('- ') for line in block.splitlines())
---
+++ src/test_block_to_block_type.py
@@ -0,0 +1,79 @@
+import unittest
+
+from block_to_block_type import *
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    def test_paragraph(self):
+        block = "This is a paragraph - albeit, very short!"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_code(self):
+        block = """```Python
+def hello(name):
+    if not name:
+        name = "world"
+
+    print(f"Hello, {name}!")
+```"""
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_quote(self):
+        block = """> I have a dream that my four little children will one day live in a nation
+> where they will not be judged by the color of their skin
+> but by the content of their character."""
+
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_unordered_list(self):
+        block = """- Eggs
+- Sausage
+- Chips
+- Beans
+"""
+
+    def test_ordered_list(self):
+        block = """1. Eggs
+2. Sausage
+3. Chips
+4. Beans
+"""
+
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.ORDERED_LIST)
+
+    def test_headings_1(self):
+        block = "# Heading level 1"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_headings_2(self):
+        block = "## Heading level 2"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_headings_3(self):
+        block = "### Heading level 3"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_headings_4(self):
+        block = "#### Heading level 4"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_headings_5(self):
+        block = "##### Heading level 5"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_headings_6(self):
+        block = "###### Heading level 6"
+        result = block_to_block_type(block)
+        self.assertEqual(result, BlockType.HEADING)
+
+
+if __name__ == "__main__":
+    unittest.main()
