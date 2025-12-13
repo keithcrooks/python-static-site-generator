7936b2a7c9440afac4ec04997384a6d658e4d1b5
Keith Crooks <keithcrooks@gmail.com>
2025-12-13 20:19:26 +0000

    Strip whitespace from block when getting block type

--- src/block_to_block_type.py
+++ src/block_to_block_type.py
@@ -12,21 +12,23 @@ class BlockType(Enum):
 HEADING_REGEX = re.compile(r"^#{1,6} [^\n]+$")
 
 def block_to_block_type(block: str) -> BlockType:
+    block = block.strip()
+
     if is_heading(block):
         return BlockType.HEADING
-    
+
     if is_code(block):
         return BlockType.CODE
-    
+
     if is_quote(block):
         return BlockType.QUOTE
-    
+
     if is_ordered_list(block):
         return BlockType.ORDERED_LIST
-    
+
     if is_unordered_list(block):
         return BlockType.UNORDERED_LIST
-    
+
     return BlockType.PARAGRAPH
 
 def is_code(block: str) -> bool:
@@ -43,7 +45,7 @@ def is_ordered_list(block :str) -> bool:
     for i, line in enumerate(lines, start=1):
         if not line.startswith(f"{i}. "):
             return False
-        
+
     return True
 
 def is_unordered_list(block :str) -> bool:
